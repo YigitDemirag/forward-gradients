c15e469c52e1c47daef6b9f7d41326c69c230ab3
Yigit Demirag <yigit@ini.uzh.ch>
2023-03-29 03:36:00 +0200

    prepared for sweep

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 import jax.numpy as jnp
-from jax import jit, random, value_and_grad, jvp, vjp
-from jax.tree_util import tree_map, tree_flatten, tree_unflatten, tree_leaves, tree_structure
+from jax import jit, random, jvp, vjp
+from jax.lax import switch
+from jax.tree_util import tree_map, tree_flatten, tree_unflatten
 from jax.example_libraries import optimizers, stax
 from jax.example_libraries.stax import Dense, LogSoftmax, Relu
 from datasets import get_train_batches, get_test_batches
 
 
-def train(key, epochs, batch_size, lr):
+def train(key, epochs, batch_size, lr, num_layers, ad_type, drct_grad_clip):
 
     def loss(params, batch):
         inputs, targets = batch
@@ -18,12 +19,12 @@ def train(key, epochs, batch_size, lr):
         target_class = jnp.argmax(targets, axis=1)
         predicted_class = jnp.argmax(predict(params, inputs), axis=1)
         return jnp.mean(predicted_class == target_class)
-
-    init_params, predict = stax.serial(
-        Dense(1024), Relu,
-        Dense(1024), Relu,
-        Dense(10), LogSoftmax)
-
+        
+    # Define model
+    layers = [Dense(1024), Relu] * num_layers
+    layers.append(Dense(10))
+    layers.append(LogSoftmax)
+    init_params, predict = stax.serial(*layers)
     _, init_rand_params = init_params(key, (-1, 28 * 28))
 
     opt_init, opt_update, get_params = optimizers.sgd(step_size=lr)
@@ -37,27 +38,32 @@ def train(key, epochs, batch_size, lr):
         v = tree_unflatten(tree_def, v_flat)
         return v
 
-    @jit
-    def update(key, i, opt_state, batch):
-        params = get_params(opt_state)
-        loss_value, grads = value_and_grad(loss)(params, batch)
-        
-        # Calculate value and loss via vjp
-        #loss_value, f_vjp = vjp(loss, params, batch)
-        #grads, _ = f_vjp(jnp.ones_like(loss_value))
-
-        # Calculate value and loss via jvp
+    def loss_and_grads_with_vjp(key, params, batch):
+        loss_value, f_vjp = vjp(loss, params, batch)
+        grads, _ = f_vjp(jnp.ones_like(loss_value))
+        return loss_value, grads, 0.0
+    
+    def loss_and_grads_with_jvp(key, params, batch):
         v = create_v(key, params)
         loss_value, drct_grad = jvp(loss, (params, batch),
                                           (v, tree_map(lambda x: jnp.zeros_like(x), batch)))
-        grads = tree_map(lambda v_leaf: v_leaf*jnp.clip(drct_grad,-1,1), v)
+        grads = tree_map(lambda v_leaf: v_leaf*jnp.clip(drct_grad,-drct_grad_clip, drct_grad_clip), v)
+        return loss_value, grads, drct_grad
+
+    @jit
+    def update(key, i, opt_state, batch, ad_type):
+        params = get_params(opt_state)
+        loss_value, grads, drct_grad = switch(ad_type, 
+                                              [loss_and_grads_with_jvp, loss_and_grads_with_vjp],
+                                              key, params, batch)
+
         return opt_update(i, grads, opt_state), loss_value, drct_grad
 
     iter_cnt = 0
-    for epoch_id in range(epochs):
+    for _ in range(epochs):
         for batch_id, batch in enumerate(get_train_batches(batch_size)):
-            key, subkey = random.split(key)
-            opt_state, loss_value, drct_grad = update(key, batch_id, opt_state, batch)
+            key, _ = random.split(key)
+            opt_state, loss_value, drct_grad = update(key, batch_id, opt_state, batch, ad_type)
             iter_cnt += 1
             if batch_id % 10 == 0:
                 params = get_params(opt_state)
@@ -74,16 +80,29 @@ if __name__ == '__main__':
     import wandb
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--lr', type=float, default=3e-4)
     parser.add_argument('--seed', type=int, default=23)
+    parser.add_argument('--num_layers', type=int, default=2)
+    parser.add_argument('--ad', type=str, default='jvp')
+    parser.add_argument('--drct_grad_clip', type=float, default=1)
 
     args = parser.parse_args()
     wandb.init(project='forward', config=args)
     wandb.config.update(args)
 
+    if args.ad == 'jvp':
+        ad_type = 0 # jvp
+    else:
+        ad_type = 1 # vjp
+
+    print('Training with {} mode'.format('jvp' if ad_type == 0 else 'vjp'))
+
     train(random.PRNGKey(args.seed),
           args.epochs,
           args.batch_size,
-          args.lr)
+          args.lr,
+          args.num_layers,
+          ad_type,
+          args.drct_grad_clip)
